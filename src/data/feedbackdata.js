d6896f5d5d130bfae8d06122410e8087e631bb4b
Malik Awais <awais.ashraf@telenorbank.pk>
2022-08-31 17:27:38 +0500

    Initial Commit

---
+++ src/data/feedbackdata.js
@@ -0,0 +1,18 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo a eveniet officiis optio, qui laborum ex molestias nesciunt maiores. Magni, cum?",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo a eveniet officiis optio, qui laborum ex molestias nesciunt maiores. Magni, cum?",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo a eveniet officiis optio, qui laborum ex molestias nesciunt maiores. Magni, cum?",
+  },
+]
+export default FeedbackData
